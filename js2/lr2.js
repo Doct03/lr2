fbd2b3fad251775a740b50b46e6c9b96332e00f3
Doct03 <84016607+Doct03@users.noreply.github.com>
2021-05-16 13:32:16 +0300

    Delete js2 directory

--- js2/lr2.js
+++
@@ -1,8 +0,0 @@
-var time;
-time=prompt("введите время");
-if (time > 17) {
-   alert('Добрый вечер')
-  }  else if (time <=17) {
-      alert('Добрый день')
-  }
-  
--- js2/lr2_2.js
+++
@@ -1,7 +0,0 @@
-var name;
-name=prompt("введите имя");
-if (name == "Jonh") {
-   alert('Привет Jonh')
-  }  else if (name != "Jonh") {
-      alert('Я вас не знаю')
-  }
